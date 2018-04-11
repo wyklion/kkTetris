438a9341cb2b9883e15d549bec5fb9ef0a382562
kk <wyklion@qq.com>
2018-04-11 20:44:45 +0800

    server

--- front/src/config.js
+++ front/src/config.js
@@ -1,8 +1,8 @@
 
 var config = {
-   host: 'localhost',
-   port: '807',
-   server: 'localhost:807',
+   //server: 'localhost:807',
+   server: '140.143.128.60:807',
+   //server: '192.168.1.112:807',
    render: {
       width: 960,
       height: 700,
